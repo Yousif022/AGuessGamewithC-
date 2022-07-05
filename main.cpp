15bdabbc3c17dcae1c1001939e414b204f2d8b51
Yousif022 <91213036+Yousif022@users.noreply.github.com>
2022-07-05 10:52:56 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int secretNum = 7;
+    int guess;
+    int guessCount = 0;
+    int guessLimit = 3;
+    bool outofGuesses = false;
+    while(secretNum != guess && !outofGuesses)
+    {
+        if(guessCount < guessLimit) {
+            cout << "Enter guess: ";
+            cin >> guess;
+            guessCount++;
+        }else{
+        outofGuesses = true;
+        }
+    }
+    if(outofGuesses){
+        cout << "You lose!";
+    }else{
+        cout << "You win!!" << endl;
+
+    }
+    return 0;
+}
